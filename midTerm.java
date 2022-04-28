8faa4584e1b4a4b6b112b77f0a772efee82437c6
FishInAZ <103294608+FishInAZ@users.noreply.github.com>
2022-04-28 15:57:27 -0700

    fail test

--- midTerm.java
+++ midTerm.java
@@ -1,2 +1,5 @@
 public class midTerm{
+    public static int minus(int x, int y){
+        return x - y;
+    }
 }
---
+++ midTermTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class midTermTest {
+
+    @Test
+    public void minus(){
+        assertEquals(2,midTerm.minus(2, 1));
+    }
+}
